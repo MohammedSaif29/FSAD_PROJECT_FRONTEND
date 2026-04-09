7adb2c95c42a002a23857a664e3da8e299ff0ea2
MohammedSaif29 <2400031209@kluniversity.in>
2026-04-09 13:47:36 +0530

    Initialize frontend repository

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: "/FSDA-PROJECT/",
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+      '/oauth2': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        xfwd: true,
+      },
+      '/login': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        xfwd: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  }
+})
